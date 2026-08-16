a2684d6bf4f9992ff0c6a614cac4db4c028f1f1b
Joaquin Kiko <joaquinkiko@proton.me>
2026-08-16 15:03:58 -0600

    initial setup

---
+++ addons/snap-gd/snapgd_plugin.gd
@@ -0,0 +1,15 @@
+@tool
+extends EditorPlugin
+
+
+func _enter_tree() -> void:
+	pass
+
+func _exit_tree() -> void:
+	pass
+
+func _enable_plugin() -> void:
+	pass
+
+func _disable_plugin() -> void:
+	pass
