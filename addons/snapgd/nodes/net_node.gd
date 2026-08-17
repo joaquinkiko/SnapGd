77ce1e3dd2ff1664e8be5b40e68c79c26367e696
Joaquin Kiko <joaquinkiko@proton.me>
2026-08-17 13:53:34 -0600

    fix: NetNode not capturing/applying properties

--- addons/snapgd/nodes/net_node.gd
+++ addons/snapgd/nodes/net_node.gd
@@ -1,10 +1,10 @@
 ## Manages what properties should be synchronized between peers
 class_name NetNode extends Node
 
-## Client -> Server properties
+## Client -> Server properties (NodePath:Property:OptionalSubProperty)
 @export var command_properties: PackedStringArray
 
-## Server -> client properties
+## Server -> client properties (NodePath:Property:OptionalSubProperty)
 @export var state_properties: PackedStringArray
 
 func _enter_tree() -> void:
@@ -16,26 +16,42 @@ func _exit_tree() -> void:
 ## Reads command properties to [param command].
 func capture_command(command: SnapCommand) -> void:
 	for prop in command_properties:
-		command.data[_key(prop)] = get_indexed(prop)
+		if prop.split(':').size() < 2: continue
+		var node: Node = get_node(prop.split(':', true, 2)[0])
+		var property: NodePath = prop.split(':', true ,2)[1]
+		if node == null || property.is_empty(): continue
+		command.data[_key(prop)] = node.get_indexed(property)
 
 ## Writes [param command] back to this node.
 func apply_command(command: SnapCommand) -> void:
 	for prop in command_properties:
 		var key := _key(prop)
 		if command.data.has(key):
-			set_indexed(prop, command.data[key])
+			if prop.split(':').size() < 2: continue
+			var node: Node = get_node(prop.split(':', true, 2)[0])
+			var property: NodePath = prop.split(':', true ,2)[1]
+			if node == null || property.is_empty(): continue
+			node.set_indexed(property, command.data[key])
 
 ## Read state properties to [param state].
 func capture_state(state: SnapState) -> void:
 	for prop in state_properties:
-		state.data[_key(prop)] = get_indexed(prop)
+		if prop.split(':').size() < 2: continue
+		var node: Node = get_node(prop.split(':', true, 2)[0])
+		var property: NodePath = prop.split(':', true ,2)[1]
+		if node == null || property.is_empty(): continue
+		state.data[_key(prop)] = node.get_indexed(property)
 
 ## Writes [param state] back to this node.
 func apply_state(state: SnapState) -> void:
 	for prop in state_properties:
 		var key := _key(prop)
 		if state.data.has(key):
-			set_indexed(prop, state.data[key])
+			if prop.split(':').size() < 2: continue
+			var node: Node = get_node(prop.split(':', true, 2)[0])
+			var property: NodePath = prop.split(':', true ,2)[1]
+			if node == null || property.is_empty(): continue
+			node.set_indexed(property, state.data[key])
 
 ## Gets reconciliation offset for visual properties.
 func get_render_offset(prop: String) -> Variant:
