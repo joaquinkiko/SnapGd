8f10c8323cd0ab5329be2bb38e277c8da33bf8db
Joaquin Kiko <joaquinkiko@proton.me>
2026-08-28 20:39:47 -0600

    chore: add data validation to NetNode

--- addons/snapgd/nodes/net_node.gd
+++ addons/snapgd/nodes/net_node.gd
@@ -23,6 +23,11 @@ signal sample_input(command: SnapCommand)
 ## Set to 0 to suppress sending, or raise to temporarily boost it.
 var priority_multiplier: float = 1.0
 
+## Cached expected Variant type per state_properties index, sampled at _ready.
+var _state_property_types: PackedInt32Array
+## Cached expected Variant type per command_properties index, sampled at _ready.
+var _command_property_types: PackedInt32Array
+
 func _enter_tree() -> void:
 	super._enter_tree()
 	SnapAPI.register_net_node(self)
@@ -34,26 +39,23 @@ func _exit_tree() -> void:
 func _ready() -> void:
 	if root == null:
 		root = self
-	for n in state_properties.size():
-		var prop := state_properties[n]
-		if prop.split(':').size() < 2:
-			push_error("Property '%s' wasn't found on '%s'"%[state_properties[n], root.name])
-			continue
-		var node: Node = root.get_node(prop.split(':', true, 2)[0])
-		var property: NodePath = prop.split(':', true, 2)[1]
-		if node == null || property.is_empty():
-			push_error("Property '%s' wasn't found on '%s'"%[state_properties[n], root.name])
-			continue
-	for n in command_properties.size():
-		var prop := command_properties[n]
-		if prop.split(':').size() < 2:
-			push_error("Property '%s' wasn't found on '%s'"%[state_properties[n], root.name])
-			continue
-		var node: Node = root.get_node(prop.split(':', true, 2)[0])
-		var property: NodePath = prop.split(':', true, 2)[1]
-		if node == null || property.is_empty():
-			push_error("Property '%s' wasn't found on '%s'"%[state_properties[n], root.name])
-			continue
+	# Validate properties and cache their types
+	_state_property_types.resize(state_properties.size())
+	_command_property_types.resize(command_properties.size())
+	for i in state_properties.size():
+		var value := _sample_property(state_properties[i])
+		if value != null:
+			_state_property_types[i] = typeof(value)
+		else:
+			_state_property_types[i] = -1
+			push_error("State property '%s' couldn't be found"%state_properties[i])
+	for i in command_properties.size():
+		var value := _sample_property(command_properties[i])
+		if value != null:
+			_command_property_types[i] = typeof(value)
+		else:
+			_command_property_types[i] = -1
+			push_error("Command property '%s' couldn't be found"%command_properties[i])
 
 ## Reads command properties to [param command].
 func capture_command(command: SnapCommand) -> void:
@@ -70,6 +72,8 @@ func apply_command(command: SnapCommand) -> void:
 	for i in command_properties.size():
 		var key := make_property_key(net_id, i)
 		if command.data.has(key):
+			if _command_property_types[i] != typeof(command.data[key]):
+				continue # Data is invalid type
 			var prop := command_properties[i]
 			if prop.split(':').size() < 2: continue
 			var node: Node = root.get_node(prop.split(':', true, 2)[0])
@@ -92,6 +96,8 @@ func apply_state(state: SnapState) -> void:
 	for i in state_properties.size():
 		var key := make_property_key(net_id, i)
 		if state.data.has(key):
+			if _state_property_types[i] != typeof(state.data[key]):
+				continue # Data is invalid type
 			var prop := state_properties[i]
 			if prop.split(':').size() < 2: continue
 			var node: Node = root.get_node(prop.split(':', true, 2)[0])
@@ -106,3 +112,10 @@ func get_render_offset(index: int) -> Variant:
 ## Clears reconciliation offset for this property, forcing an instant snap.
 func clear_render_offset(index: int) -> void:
 	SnapAPI.render_offsets.erase(make_property_key(net_id, index))
+
+func _sample_property(prop: String) -> Variant:
+	if prop.split(':').size() < 2: return null
+	var node: Node = root.get_node(prop.split(':', true, 2)[0])
+	var property: NodePath = prop.split(':', true, 2)[1]
+	if node == null || property.is_empty(): return null
+	return node.get_indexed(property)
